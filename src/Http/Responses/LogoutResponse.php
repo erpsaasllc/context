e25836a2675500949be52262f1bed9b7d2e1b75b
Andrew Wallo <erpsaasllc@gmail.com>
2022-12-11 20:15:08 -0500

    new file:   src/Http/Responses/LogoutResponse.php 	modified:   stubs/config/context.php

---
+++ src/Http/Responses/LogoutResponse.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ERPSAAS\Context\Http\Responses;
+
+use Filament\Http\Responses\Auth\Contracts\LogoutResponse as LogoutResponseContract;
+
+class LogoutResponse implements LogoutResponseContract
+{
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function toResponse($request)
+    {
+        return $request->wantsJson()
+                    ? response()->json(['two_factor' => false])
+                    : redirect()->intended(config('context.redirects.logout'));
+    }
+}
